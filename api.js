be8e41ce8da38aaae6d38e5fec2a7533c2be0993
AkshithaBannan <143248373+AkshithaBannan@users.noreply.github.com>
2025-09-21 23:15:00 -0500

    Update api.js

--- api.js
+++ api.js
@@ -116,3 +116,34 @@ async function deleteProduct (req, res, next) {
   const response = await Products.destroy(req.params.id)
   res.json(response)
 }
+// api.js
+
+/**
+ * Create an order
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(orders)
+}
+
+/**
+ * List orders
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+}
